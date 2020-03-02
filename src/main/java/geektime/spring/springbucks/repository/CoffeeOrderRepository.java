898124b79415c942b96d4d96ac030e0696fa9806
chzhyang <chzhyangchn@gmail.com>
2020-03-02 21:28:53 +0800

    JPA

--- src/main/java/geektime/spring/springbucks/repository/CoffeeOrderRepository.java
+++ src/main/java/geektime/spring/springbucks/repository/CoffeeOrderRepository.java
@@ -3,9 +3,5 @@ package geektime.spring.springbucks.repository;
 import geektime.spring.springbucks.model.CoffeeOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
-public interface CoffeeOrderRepository extends BaseRepository<CoffeeOrder, Long> {
-    List<CoffeeOrder> findByCustomerOrderById(String customer);
-    List<CoffeeOrder> findByItems_Name(String name);
+public interface CoffeeOrderRepository extends JpaRepository<CoffeeOrder, Long> {
 }
